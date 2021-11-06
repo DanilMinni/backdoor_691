f1ae70aafd817c7fb07d9bb110d0ae9b86080606
Koranel <distrees@mail.ru>
2021-11-06 20:35:16 +0300

    06.11.21

--- app/src/main/java/com/example/ework/Chat.java
+++ app/src/main/java/com/example/ework/Chat.java
@@ -3,22 +3,31 @@ package com.example.ework;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import android.text.format.DateFormat;
 
 
 public class Chat extends AppCompatActivity {
     private static int SIGN_IN_CODE=1;
     private RelativeLayout chat_layout;
     private FirebaseListAdapter<Massage> adapter;
+    private FloatingActionButton sendBtn;
+    private  String massage;
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -40,6 +49,17 @@ public class Chat extends AppCompatActivity {
         setContentView(R.layout.chat_layout);
 
         chat_layout=findViewById(R.id.chat_layout);
+        sendBtn=findViewById(R.id.btnSend);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText textField = findViewById(R.id.massageFild);
+
+                if( textField.getText().toString() == "") return;
+                FirebaseDatabase.getInstance().getReference().push().setValue(new Massage(FirebaseAuth.getInstance().getCurrentUser().getEmail(),textField.getText().toString()));
+                textField.setText(massage);
+            }
+        });
 
         //не авторизован
         if(FirebaseAuth.getInstance().getCurrentUser() == null)
@@ -52,5 +72,20 @@ public class Chat extends AppCompatActivity {
 
     private void displayAllMassages() {
         ListView listOfMassages = findViewById(R.id.list_of_massages);
+        adapter = new FirebaseListAdapter<Massage>(this,Massage.class,R.layout.list_item, FirebaseDatabase.getInstance().getReference()) {
+            @Override
+            protected void populateView(View v, Massage model, int position) {
+                TextView mess_user,mess_time,mess_text;
+                mess_user = v.findViewById(R.id.massage_user);
+                mess_time = v.findViewById(R.id.massage_time);
+                mess_text = v.findViewById(R.id.massage_text);
+
+                mess_user.setText(model.getUserName());
+                mess_text.setText(model.getTextMassage());
+                mess_time.setText(DateFormat.format("dd-mm-yyyy HH:mm:ss", model.getMessageTime()));
+
+            }
+        };
+        listOfMassages.setAdapter(adapter);
     }
 }
